1dd350565ab0763b8ea03fde5dce621111667a28
Livre progamacao ltda-me <livre.programacao@gmail.com>
2012-06-21 10:09:42 -0300

    Adicionado nova dependencia para usar os temas do primeface; Adicionado o web.xml para definir o tema de forma estatitica; Adicionado o .gitignore; Atualizado o import.sql.
    Signed-off-by: Livre progamacao ltda-me <livre.programacao@gmail.com>

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,5 +4,5 @@ insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'joh
 --create schema if not exists "CONDOMINIO";
 
 --CALL CSVWRITE('/home/oracle/workspace/condominio/src/main/resources/unidade.csv', 'SELECT * FROM CONDOMINIO.UNIDADE');
-insert into UNIDADE (id, bloco, unidade, tipo, fracao, nome) values (0, 0, 0, 'A', 0, 'TESTE');
+--insert into UNIDADE (id, bloco, unidade, tipo, fracao, nome) values (0, 0, 0, 'A', 0, 'TESTE');
 insert into UNIDADE (id, bloco, unidade, tipo, fracao, nome) select * from CSVREAD('/home/oracle/workspace/condominio/src/main/resources/unidade.csv');
